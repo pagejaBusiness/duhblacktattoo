0ea840a4e69a60bbc6b9454aaa2d8fd3b13c81ea
WilliamAraujo <w67543210@gmail.com>
2025-09-22 18:42:51 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <p className="text-example">app</p>
+      <p className="text-example">Sua página começa aqui</p>
     </div>
   );
 }
