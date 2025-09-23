000349deb4553d1afc4809c69a028649f13738b6
WilliamAraujo <w67543210@gmail.com>
2025-09-22 21:45:51 -0300

    add:tema padrão do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        example: "#03903c",
+        dark: {
+          DEFAULT: "#0B0B0F", // fundo principal quase preto
+          lighter: "#1A1A1F", // cards / elementos destacados
+        },
+        purple: {
+          DEFAULT: "#7C3AED", // roxo principal (vibrante e moderno)
+          light: "#9F67FF", // hover e destaques
+          dark: "#5B21B6", // para botões ou títulos mais fortes
+        },
+        white: {
+          DEFAULT: "#F9FAFB", // branco suave para não cansar
+          pure: "#FFFFFF", // branco puro para textos importantes
+        },
+        gray: {
+          DEFAULT: "#9CA3AF", // cinza médio (textos secundários)
+          dark: "#6B7280", // cinza mais escuro para balancear
+        },
       },
     },
   },
